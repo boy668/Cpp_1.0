2748e040fd8ae8fe82d33eaff743d0089a23f745
boy668 <1736808015@qq.com>
2026-09-22 22:49:10 +0800

    新增B_1.1、beginner_1.0两套练习代码

---
+++ B_1.1/Cpp_B1.1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>//输入输出流 
+using namespace std; //命名空间 
+//
+
+
+//int main() {
+//	int n;
+//	cin >> n;
+//	cout << n<<endl; 
+//	
+//	
+//	return 0;
+//}
+//int main()
+//{
+// char c1 = 'Q';
+// cout << c1 << endl;
+// char c2 = 81;
+// cout << c2 << endl; //这?的81是字符Q的ASCII码值，也是可以正常打印的
+// return 0;
+//}
+
+
+//Length of each data type
+//int main (){
+//	long long a=0;
+//	cout << sizeof(a)<<endl;
+//	cout << sizeof(char) << endl;
+// cout << sizeof(bool) << endl;
+// cout << sizeof(short) << endl;
+// cout << sizeof(int) << endl;
+// cout << sizeof(long) << endl;
+// cout << sizeof(long long) << endl;
+// cout << sizeof(float) << endl;
+// cout << sizeof(double) << endl;
+// cout << sizeof(long double) << endl;
+//	
+//	
+//	return 0;
+//} 
+
+
+
+
+
